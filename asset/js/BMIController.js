e86d67edff0114b9777a45300dfbbcee7cf63c6a
liipe <luiz-dasilva@hotmail.com>
2015-09-10 14:56:11 +0000

    Added any modifications

--- asset/js/BMIController.js
+++ asset/js/BMIController.js
@@ -19,23 +19,15 @@ var BMIController = {
 			height = parseFloat(form.height.value),
 			result = 0;
 		
-		var callback = function(result) {
-			BMIController.showResult(result);			
-		};
+		result = BMIService.getIndex(weight, height);
 		
-		BMIController.showLoading(true);
-		BMIService.getIndex(weight, height, callback);
+		BMIController.showResult(result);
 	},
 	
 	showResult: function(result) {
 		var spanResult = document.querySelector('.result');
 		spanResult.innerHTML = result.toFixed(2);
-		BMIController.showLoading(false);
 	},
-	
-	showLoading: function(isLoading) {
-		document.querySelector('.label').innerHTML = isLoading ? 'loading...' : 'BMI Result'
-	}
 
 };
 
